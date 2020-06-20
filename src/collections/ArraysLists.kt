92f2d43f7cafa507f5aecf56def8ff3ac2df0f67
muzaffar.makhkamov <maxalphaengineer@gmail.com>
2020-06-21 00:11:06 +0500

    -Lists

--- src/collections/ArraysLists.kt
+++ src/collections/ArraysLists.kt
@@ -8,7 +8,7 @@ fun main(args: Array<String>) {
     val mixed = arrayOf("Kotlin", 17, 3.2, false)
     val numbers = intArrayOf(1,2,3,4,5)
 
-    println(mixed[2])
+   /* println(mixed[2])
 
     mixed[2] = 3.14
     println(mixed[2])
@@ -29,7 +29,28 @@ fun main(args: Array<String>) {
         println("Chortoqliklar bormi?")
     }else{
         println("Ha maylida gaplawamiz!")
-    }
+    }*/
 
 
+    val arrayList = arrayListOf("Muzaffar","Murodjon", "Jahongir")
+    println(arrayList[0])
+
+    val list = arrayListOf("Abdullo")
+
+    println(arrayList+list)
+    println(arrayList.size)
+    println(arrayList.isEmpty())
+    println(arrayList.contains("Murodjon"))
+
+    val added =  arrayList.add("Azizbek")
+    println(arrayList)
+     arrayList.add(2,"Sirojiddin")
+    println(arrayList)
+    println(added)
+
+    val removed = arrayList.remove("Azizbek")
+    println(removed)
+
+    val sublist = arrayList.subList(1,4)
+    println(sublist)
 }
