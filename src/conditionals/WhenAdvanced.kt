d9aa9ca072965c6d930c22137327a9cffc11b40f
muzaffar.makhkamov <maxalphaengineer@gmail.com>
2020-06-20 21:20:38 +0500

    -When advanced

---
+++ src/conditionals/WhenAdvanced.kt
@@ -0,0 +1,12 @@
+package conditionals
+
+fun main(args: Array<String>) {
+    val x = 11
+    when(x){
+        5-> println("x: "+5 )
+        3*12-> println("x: "+3*12 )
+        "Hey there".length-> println("x is equals stirng 'Hey there' "+"Hey there".length )
+        !in 1..9-> println("x is not between 1 and 9")
+        in 1..10-> println("x is between 1 and 10")
+    }
+}
