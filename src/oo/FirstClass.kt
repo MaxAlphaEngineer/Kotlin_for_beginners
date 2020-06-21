845fdc424021b127c6a0afab9d2e2ada1cbfa651
muzaffar.makhkamov <maxalphaengineer@gmail.com>
2020-06-21 09:03:35 +0500

    -Methods

--- src/oo/FirstClass.kt
+++ src/oo/FirstClass.kt
@@ -3,6 +3,18 @@ package oo
 class Person {
     var name: String = "Muzaffar"
     var age: Int = 25
+
+    fun speak(){
+        println("Hello darkness :<")
+    }
+    fun greet(name: String){
+        println("Hello $name!")
+    }
+
+    fun getYearOfBirth() = 2020 -age
+//    fun getYearOfBirth():Int{
+//        return 2020-age
+//    }
 }
 
 fun main(args: Array<String>) {
@@ -10,7 +22,13 @@ fun main(args: Array<String>) {
     println(person.name)
     println(person.age)
 
+    //calling method
+    person.speak()
+    person.greet("my friend")
 
+    println(person.getYearOfBirth())
+
+    //re-assign class parameters
     person.name = "Azamat"
     person.age = 20
     println(person.name)
